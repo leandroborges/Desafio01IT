35ecdd7ad024f98c966ed8aaaf5050ac513afea5
Leandro Borges <leandro.borges@prestadores.uhgbrasil.com.br>
2022-09-29 22:31:20 -0300

    feat: add controllers

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,44 @@
+﻿using Desafio01IT.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio01IT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly ILogger<TimeController> _logger;
+
+        public TimeController(ILogger<TimeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet()]
+        public IEnumerable<Time> List()
+        {
+            return null;
+        }
+    }
+}
